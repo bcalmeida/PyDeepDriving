cb63ea18051135d6416c13987f6c8e83477bad14
Bruno Almeida <bruno.correia.almeida@gmail.com>
2018-10-26 16:07:09 -0300

    Add BatchifiedFeedbackDataset

--- drivenet/data_handlers.py
+++ drivenet/data_handlers.py
@@ -36,6 +36,31 @@ class BatchifiedDataset(Dataset):
             return BatchSampler(RandomSampler(self), batch_size=self.bs*self.seq_len, drop_last=True)
         return BatchSampler(SequentialSampler(self), batch_size=self.bs*self.seq_len, drop_last=True)
 
+class BatchifiedFeedbackDataset(Dataset):
+    def __init__(self, data, bs, seq_len, transform, PATH, IMAGES_FOLDER):
+        bdata = batchify(data, bs)
+        self.bdata = bdata.reshape(bdata.shape[0]*bdata.shape[1], *bdata.shape[2:])  # shape (len(data)//bs, bs, *) => (len(data), *)
+        self.bs = bs
+        self.seq_len = seq_len
+        self.transform = transform
+        self.PATH = PATH
+        self.IMAGES_FOLDER = IMAGES_FOLDER
+
+    def __len__(self):
+        return len(self.bdata)
+
+    def __getitem__(self, i):
+        img_id = self.bdata[i][0]
+        img = open_image(os.path.join(self.PATH, self.IMAGES_FOLDER, img_id + '.png'))
+        y = self.bdata[i][1:].astype(np.float32)
+        img, _ = self.transform(img, y) # No transformation on y supported at the moment
+        y_prev = self.bdata[i-1][1:].astype(np.float32)
+        return img, y_prev, y
+
+    def batch_sampler(self):
+        # Skips first element
+        return BatchSampler(range(1, len(self)), batch_size=self.bs*self.seq_len, drop_last=True)
+
 class LinearFeedbackDataset(Dataset):
     def __init__(self, df, transform, PATH, IMAGES_FOLDER):
         self.data = df.values
