589d7cd3cdd747a627ed3ea5a356b6e2cb3a5662
Bruno Almeida <bruno.correia.almeida@gmail.com>
2018-11-03 20:04:59 -0300

    Add CNNtoRNN net

--- drivenet/cnn_rnn_net.py
+++ drivenet/cnn_rnn_net.py
@@ -92,3 +92,77 @@ class CNNtoRNNFeedback(nn.Module):
 
     def reset(self):
         self.h, self.c = self.init_hidden()
+
+class CNNtoRNN(nn.Module):
+    def __init__(self, encode_size, hidden_size, num_layers, bs, output_size):
+        super().__init__()
+        self.encode_size = encode_size
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.bs = bs
+        self.output_size = output_size
+        
+        resnet_model = resnet34(pretrained=True)
+        encoder_layers = list(resnet_model.children())[:8] + [AdaptiveConcatPool2d(), Flatten()]
+        self.encoder = nn.Sequential(*encoder_layers).cuda()
+        for param in self.encoder.parameters():
+            param.requires_grad = False
+        set_trainable(self.encoder, False) # fastai fit bug
+        
+        self.encoder_linear = nn.Sequential(
+            nn.BatchNorm1d(1024),
+            nn.Dropout(p=0.25),
+            nn.Linear(1024, 512),
+            nn.ReLU(),
+            nn.BatchNorm1d(512),
+            nn.Dropout(p=0.5),
+            nn.Linear(512, encode_size)).cuda()
+        set_trainable(self.encoder_linear, True) # fastai fit bug
+        
+        self.lstm = nn.LSTM(encode_size, hidden_size, num_layers).cuda()
+        self.h, self.c = self.init_hidden()
+        set_trainable(self.lstm, True) # fastai fit bug
+        
+        self.linear = nn.Linear(hidden_size, output_size).cuda()
+        set_trainable(self.linear, True) # fastai fit bug
+        
+        self.init_weights()
+        
+    # added nb-23
+    def init_weights(self):
+        # self.encoder_linear
+        apply_init(self.encoder_linear, nn.init.kaiming_normal)
+        
+        # self.lstm
+        for layer_weights in self.lstm.all_weights:
+            weight_ih, weight_hh, bias_ih, bias_hh = layer_weights
+            nn.init.xavier_normal(weight_ih)
+            nn.init.xavier_normal(weight_hh)
+            bias_ih.data.fill_(0.)
+            bias_hh.data.fill_(0.)
+        
+        # self.linear
+        apply_init(self.linear, nn.init.kaiming_normal)
+        
+    def init_hidden(self):
+        # print("hidden state initialized")
+        return (Variable(T(torch.zeros(self.num_layers, self.bs, self.hidden_size))),
+                Variable(T(torch.zeros(self.num_layers, self.bs, self.hidden_size)))) # T to put in gpu
+        
+    def forward(self, x):
+        # truncated backprop
+        self.h = repackage_hidden(self.h)
+        self.c = repackage_hidden(self.c)
+        
+        # x has shape (seq_len*bs, *img_shape), and was previously windowed (seq_len, bs, *img_shape)
+        encodes = self.encoder_linear(self.encoder(x)) # shape (seq_len*bs, 1024) -> (seq_len*bs, encode_size)
+        encodes = encodes.view(-1, self.bs, self.encode_size) # shape (seq_len, bs, encode_size), could be an incomplete sequence
+
+        output, (self.h, self.c) = self.lstm(encodes, (self.h, self.c)) # output shape (seq_len, bs, hidden_size) 
+        output = output.view(-1, self.hidden_size) # output shape (seq_len*bs, hidden_size)
+
+        # y shape (seq_len*bs, output_size)
+        return self.linear(output)
+    
+    def reset(self):
+        self.h, self.c = self.init_hidden()
